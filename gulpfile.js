8a68a256a421bd9ce22bdfd7986705f43459219b
Rob LaFeve <rob@cremalab.com>
2015-09-05 22:37:49 -0500

    Add watch tasks for basic file types

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,61 @@
-var gulp = require('gulp');
+var gulp          = require('gulp');
+var autoprefixer  = require('gulp-autoprefixer');
+var concat        = require('gulp-concat');
+var ngTemp        = require('gulp-angular-templatecache');
+var babel         = require('gulp-babel');
+var stream        = require('streamqueue');
 
-gulp.task('default', function(){
-  // Default task code
+var paths = {
+  src: {
+    index: 'src/index.html',
+    js:    'src/**/*.js',
+    temps: ['src/**/*.html', '!src/index.html'],
+    css:   'src/**/*.css',
+  },
+  dest: {
+    js:  'index.js',
+    css: 'index.css',
+    dir: 'app/',
+  },
+}
+
+
+// Builds
+
+gulp.task('index', function(){
+  gulp.src(paths.src.index)
+    .pipe(gulp.dest(paths.dest.dir))
 });
+
+gulp.task('js', function(){
+  stream(
+    { objectMode: true },
+    gulp.src(paths.src.js),
+    gulp.src(paths.src.temps)
+      .pipe(ngTemp({standalone: true}))
+  )
+    .pipe(babel())
+    .pipe(concat(paths.dest.js))
+    .pipe(gulp.dest(paths.dest.dir))
+});
+
+gulp.task('css', function(){
+  gulp.src(paths.src.css)
+    .pipe(concat(paths.dest.css))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(paths.dest.dir))
+});
+
+
+// Watch
+
+gulp.task('watch', function(){
+  gulp.watch(paths.src.index, ['index']);
+  gulp.watch(paths.src.js, ['js']);
+  gulp.watch(paths.src.css, ['css']);
+});
+
+
+// Default
+
+gulp.task('default', ['watch']);
---
+++ src/base.js
@@ -0,0 +1,4 @@
+var test = (a) => a + 1
+var test2 = (a) => {
+  console.log("mom");
+}
